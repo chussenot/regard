b2210157200dd595a611c65fadeed25a6e4a185c
Clement Hussenot <clement.hussenot@manomano.com>
2019-07-22 23:05:14 +0200

    The commands will return Results with sometimes vulns

--- lib/regard.rb
+++ lib/regard.rb
@@ -8,4 +8,30 @@ require 'regard/module_injector'
 require 'regard/modules/abstract_discover'
 
 module Regard
+
+  class Result
+    attr_accessor :state, :output, :obj, :vuln
+
+    def initialize
+      state = :untested
+      vuln = Regard::Vuln.new
+    end
+  end
+
+  class Vuln
+    attr_accessor :severity, :description, :solution,
+                  :title, :tags, :references, :risk, :reflinks
+
+    def initialize
+      severity = 0
+      title = 'Forgot to add title'
+      description = 'Forgot to add description'
+      solution = 'Information only.'
+      tags = {}
+      references = {}
+      risk = {}
+      reflinks = {}
+    end
+  end
+
 end
